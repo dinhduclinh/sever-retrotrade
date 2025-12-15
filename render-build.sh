36a299546acb611ea869aed9e77ba2c5bbb78a0c
dinhduclinh <dinhduclinh6823@gmail.com>
2025-12-15 16:11:34 +0700

    fixx

--- render-build.sh
+++ render-build.sh
@@ -5,10 +5,17 @@ export PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
 
 mkdir -p $PUPPETEER_CACHE_DIR
 
-# Install Chrome latest vào cache persist
-npx @puppeteer/browsers install chrome@latest --path $PUPPETEER_CACHE_DIR
+# Copy cache nếu có từ deploy trước, hoặc store cho lần sau
+if [[ ! -d $PUPPETEER_CACHE_DIR/chrome ]]; then
+  echo "...Copying Puppeteer Cache from actual storage"
+  mkdir -p $PUPPETEER_CACHE_DIR
+  cp -R /opt/render/project/src/.cache/puppeteer/chrome/* $PUPPETEER_CACHE_DIR/ || true
+else
+  echo "...Storing Puppeteer Cache for next deploy"
+  cp -R $PUPPETEER_CACHE_DIR/chrome/* /opt/render/project/src/.cache/puppeteer/chrome/ || true
+fi
 
 npm install
 
-# Nếu có build khác (NestJS build...)
+# Nếu có lệnh build khác thì thêm ở đây
 # npm run build
